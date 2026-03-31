51d7d7b739ac3cb83a4674cd5ea7ec6ee525efcd
Imane <imane.cours69800@gmail.com>
2026-03-31 15:06:39 +0200

    seperated in different files the logic in src

--- include/LoadCell.h
+++ include/LoadCell.h
@@ -7,13 +7,16 @@
 class LoadCell {
   private:
     HX711 scale;
+    int _doutPin;      
+    int _sckPin;
     float calibration_factor;
     float samples[11];              // Array to store multiple readings for post-processing
     float emaValue;                 // Variable to store the current EMA value
     const float EMA_ALPHA = 0.1;    // Smoothing factor for EMA
 
   public:
-    LoadCell(int doutPin, int sckPin, float calibFactor) : scale(doutPin, sckPin), calibration_factor(calibFactor) {}
+    LoadCell(int doutPin, int sckPin, float calibFactor) 
+        : _doutPin(doutPin), _sckPin(sckPin), calibration_factor(calibFactor) {}
 
     void begin();       // Initializes the HX711 and sets the calibration factor
     void measureWeight();   // Handles reading, median filtering, and EMA calculation
